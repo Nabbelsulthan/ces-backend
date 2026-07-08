0f4f27ee563c51db05983ad700b85158f0506b82
NABBEL SULTHAN <nabbelmech@gmail.com>
2026-07-08 17:55:50 +0530

    project fetch fix2

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -170,7 +170,7 @@ router.put("/:id", async (req, res) => {
 });
 
 
-router.get("/:id", authenticateCustomer, async (req, res) => {
+router.get("/:id", async (req, res) => {
     try {
 
         const { id } = req.params;
