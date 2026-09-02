a72082acd62fad03c9bec031e6fecabab8869c12
NABBEL SULTHAN <nabbelmech@gmail.com>
2026-09-02 14:06:52 +0530

    delte route protect

--- src/routes/documentRoutes.js
+++ src/routes/documentRoutes.js
@@ -2,8 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const authenticateAdminOrCustomer =
-    require("../middleware/authenticateAdminOrCustomer");
+
 const pool = require("../config/db");
 
 // const authenticateCustomer =
